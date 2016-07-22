c25a001c5ba59bd1b48df102ca7553e400a2c3af
baolyang <baolyang@ebay.com>
2016-07-22 09:25:22 +0800

    delete

--- ########execises/Design resources/ebay design/ebay AR/Models_Code/necklace/gen/com/ebayAR/necklace/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.ebayAR.necklace;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- ########execises/Design resources/ebay design/ebay AR/Models_Code/table/gen/m/ebayAR/table/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package m.ebayAR.table;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
